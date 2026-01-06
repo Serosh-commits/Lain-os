cd346f45cf00518a84af45bf80f2813f05264007
Serosh-commits <janmejayapanda400@gmail.com>
2026-01-06 22:14:52 +0530

    Implement kernel main entry and architecture declarations

---
+++ abyss/kernel.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
+
+namespace abyss {
+    class TemporalScheduler;
+    class NeuroVFS;
+    class EgoManager;
+    class QuantumDriver;
+    class PredictiveShell;
+    class PageTableManager;
+    class Decompiler;
+    class HotPatcher;
+
+    extern TemporalScheduler scheduler;
+    extern NeuroVFS vfs;
+    extern EgoManager ego_manager;
+    extern QuantumDriver quantum_driver;
+    extern PredictiveShell shell;
+    extern PageTableManager page_table_manager;
+    extern Decompiler decompiler;
+    extern HotPatcher hot_patcher;
+
+    extern int proc_count;
+
+    extern "C" void panic(const char* message);
+    extern "C" void syscall_ascend();
+}
---
+++ abyss/kmain.cpp
@@ -0,0 +1,49 @@
+#include "kernel.hpp"
+#include "stl/runtime.cpp"
+#include "kernel/panic.cpp"
+#include "mm/heap.cpp"
+#include "kernel/ascension.cpp"
+#include "kernel/scheduler.cpp"
+#include "fs/neurovfs.cpp"
+#include "kernel/ego.cpp"
+#include "drivers/quantum.cpp"
+#include "kernel/shell.cpp"
+#include "mm/paging.cpp"
+#include "kernel/patcher.cpp"
+
+namespace abyss {
+TemporalScheduler scheduler;
+NeuroVFS vfs;
+EgoManager ego_manager;
+QuantumDriver quantum_driver;
+PredictiveShell shell;
+PageTableManager page_table_manager;
+Decompiler decompiler;
+HotPatcher hot_patcher;
+}
+
+extern "C" void kmain() {
+    using namespace abyss;
+    volatile char* uart = (volatile char*)0x10000000;
+    const char* msg = "LainOS/Abyss: Connection to the Wired established.\n";
+    while (*msg) *uart = *msg++;
+
+    scheduler.init();
+    ego_manager.init();
+    quantum_driver.init();
+    shell.init();
+    page_table_manager.init();
+
+    const char* prompt = "\nroot@abyss# ";
+    const char* p = prompt;
+    while (*p) *uart = *p++;
+
+    for (;;) {
+        scheduler.schedule();
+        volatile char* uart_lsr = (volatile char*)0x10000005;
+        if (*uart_lsr & 1) {
+            char c = *(volatile char*)0x10000000;
+            shell.syscall_key_event(c);
+        }
+    }
+}
