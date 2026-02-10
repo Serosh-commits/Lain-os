d74a1c58f9d89bc769115dfcecd260efbbfd4ee5
Serosh <janmejayapanda400@gmail.com>
2026-02-10 23:11:52 +0530

    added string

---
+++ include/lib/string.h
@@ -0,0 +1,19 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include "lib/types.h"
+
+void* memset(void* dst, int c, size_t n);
+void* memcpy(void* dst, const void* src, size_t n);
+void* memmove(void* dst, const void* src, size_t n);
+int memcmp(const void* v1, const void* v2, size_t n);
+
+size_t strlen(const char* s);
+char* strcpy(char* dst, const char* src);
+char* strncpy(char* dst, const char* src, size_t n);
+int strcmp(const char* s1, const char* s2);
+int strncmp(const char* s1, const char* s2, size_t n);
+char* strchr(const char* s, int c);
+char* strcat(char* dst, const char* src);
+
+#endif
