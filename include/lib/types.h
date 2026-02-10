adb82e16dcf53537937bd50cb24ad097176378d9
Serosh <janmejayapanda400@gmail.com>
2026-02-10 23:11:52 +0530

    added types

---
+++ include/lib/types.h
@@ -0,0 +1,25 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+
+typedef char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef long int64_t;
+
+typedef uint64_t size_t;
+typedef int64_t ssize_t;
+typedef int64_t off_t;
+
+typedef int pid_t;
+
+#define NULL ((void*)0)
+#define true 1
+#define false 0
+typedef int bool;
+
+#endif
