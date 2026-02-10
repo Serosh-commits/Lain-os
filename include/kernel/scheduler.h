03bc7f9e45fa2a8d3b4fc508593e36e83d16d531
Serosh <janmejayapanda400@gmail.com>
2026-02-10 23:11:52 +0530

    added scheduler

---
+++ include/kernel/scheduler.h
@@ -0,0 +1,21 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include "lib/types.h"
+
+struct timeline {
+    uint64_t id;
+    uint64_t priority;
+    uint64_t quantum;
+    uint64_t elapsed;
+    struct timeline* next;
+};
+
+void sched_init();
+void sched_yield();
+void sched_timer();
+void sched_ret();
+struct timeline* timeline_create(uint64_t priority);
+void timeline_assign(int pid, uint64_t timeline_id);
+
+#endif
