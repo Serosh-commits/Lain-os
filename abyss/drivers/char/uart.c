fc423c2b430252fb99f8740a626db9c5db03d930
Serosh <janmejayapanda400@gmail.com>
2026-02-10 23:11:52 +0530

    added uart

---
+++ abyss/drivers/char/uart.c
@@ -0,0 +1,72 @@
+#include "drivers/uart.h"
+#include "kernel/riscv.h"
+#include "lib/types.h"
+
+#define RHR 0
+#define THR 0
+#define IER 1
+#define FCR 2
+#define ISR 2
+#define LCR 3
+#define LSR 5
+
+#define LSR_RX_READY (1 << 0)
+#define LSR_TX_IDLE (1 << 5)
+
+static volatile uint8_t* uart = (volatile uint8_t*)UART0;
+
+void uart_init() {
+    uart[IER] = 0x00;
+    uart[FCR] = 0x01;
+    uart[LCR] = 0x03;
+    uart[IER] = 0x01;
+}
+
+void uart_putc(int c) {
+    while ((uart[LSR] & LSR_TX_IDLE) == 0)
+        ;
+    uart[THR] = c;
+}
+
+int uart_getc() {
+    if (uart[LSR] & LSR_RX_READY) {
+        return uart[RHR];
+    }
+    return -1;
+}
+
+void uart_puts(const char* s) {
+    while (*s) {
+        if (*s == '\n') {
+            uart_putc('\r');
+        }
+        uart_putc(*s++);
+    }
+}
+
+void uart_putnum(uint64_t x, int base) {
+    static char digits[] = "0123456789abcdef";
+    char buf[65];
+    int i = 0;
+    
+    if (x == 0) {
+        uart_putc('0');
+        return;
+    }
+    
+    while (x != 0) {
+        buf[i++] = digits[x % base];
+        x /= base;
+    }
+    
+    while (i > 0) {
+        uart_putc(buf[--i]);
+    }
+}
+
+void uart_puthex(uint64_t x) {
+    uart_puts("0x");
+    for (int i = 60; i >= 0; i -= 4) {
+        uart_putc("0123456789abcdef"[(x >> i) & 0xf]);
+    }
+}
